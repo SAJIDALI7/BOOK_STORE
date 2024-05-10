d63fd2a46c32400d7adc042d9b2050d0b099bb4f
SAJIDALI7 <99169849+SAJIDALI7@users.noreply.github.com>
2024-05-10 18:58:45 +0530

    Update settings.py

--- book_store/settings.py
+++ book_store/settings.py
@@ -130,10 +130,4 @@ LOGIN_URL = 'login'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Email
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'sajidalic.actowiz@gmail.com'
-EMAIL_HOST_PASSWORD = 'uuyxfythwbqjfmcr'
+
